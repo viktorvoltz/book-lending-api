8312710ab6b617f8ecb56b6011ccbb19e5323353
viktorvoltz <vctrchinyeaka@gmail.com>
2021-10-31 23:33:06 +0100

    initial commit

--- user/user.js
+++ user/user.js
@@ -1,10 +1,8 @@
-//import { libraryIdGenerator } from '../lib/helper';
 const random = require('../lib/helper');
 
 class User {
     constructor(username) {
         this.username = username;
-        //this.password = password;
         this.id = random.generateRandomString(5);
         this.collection = [];
     }
